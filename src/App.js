9c06515d5ba7f5c21d3c727c954b215ab56f736f
Oshalusi John <oshalusijohn@gmail.com>
2020-10-28 19:10:25 +0100

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,13 @@
-import logo from './logo.svg';
+import React from "react";
 import './App.css';
 
-function App() {
+const App = () => {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      Here we are
+      <button className="btn btn-primary">Welcome</button>
     </div>
   );
-}
+};
 
 export default App;
